632b6bb90b5e37b3927ef4893acbaeea03eb1340
oatkrunch <suyalsahukhala787@gmail.com>
2025-12-04 08:31:39 +0545

    SplashScreencreated

---
+++ app/src/main/java/com/example/trailmate/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.trailmate
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFrom
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.trailmate.ui.theme.Black
+import com.example.trailmate.ui.theme.Pink
+import com.example.trailmate.ui.theme.TrailMateTheme
+import com.example.trailmate.ui.theme.White
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MainBody()
+        }
+    }
+}
+
+@Composable
+fun MainBody(){
+  Scaffold (
+      modifier = Modifier.fillMaxSize()
+  ){ padding ->
+      Column (
+          modifier = Modifier
+              .background(White)
+              .padding(padding)
+              .fillMaxSize(),
+          verticalArrangement = Arrangement.Center,
+          horizontalAlignment = Alignment.CenterHorizontally
+      ){
+         Text("TrailMate",
+             modifier = Modifier
+                 .fillMaxWidth(),
+             style = TextStyle(
+                 fontSize = 36.sp,
+                 fontWeight = FontWeight.SemiBold,
+                 textAlign = TextAlign.Center
+             ))
+          Spacer(modifier = Modifier.padding(vertical =8.dp))
+          Text("Find trusted guides anywhere.",
+              modifier = Modifier.fillMaxWidth(),
+              style = TextStyle(
+                  fontSize = 15.sp,
+                  textAlign = TextAlign.Center
+              )
+          )
+          Spacer(modifier = Modifier.padding(vertical= 10.dp))
+          Button(
+              onClick = {},
+              modifier = Modifier
+                  .width(210.dp)
+                  .height(45.dp),
+              shape = RoundedCornerShape(14.dp),
+              colors = ButtonDefaults.buttonColors(
+                  containerColor = Pink
+              )
+              ) {
+              Text("Get Started",
+                  style = TextStyle(
+                      fontSize = 14.sp,
+                      fontWeight = FontWeight.SemiBold
+                  ))
+          }
+      }
+
+  }
+}
+
+@Preview
+@Composable
+fun PreviewMainBody(){
+    MainBody()
+}
---
+++ app/src/main/java/com/example/trailmate/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.trailmate.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Pink = Color(0xFFFF385C)
